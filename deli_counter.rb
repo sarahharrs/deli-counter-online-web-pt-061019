23c5e07af7a3b069264c03385eb157faf46ceb37
Sarah Harrs <sarahharrs@gmail.com>
2019-06-28 14:05:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+Katz_deli = []
+def lin(Katz_deli) 
+  if Katz_deli = nil 
+    puts "There is nobody waiting to be served!"
+  end
+  def take_a_number(Katz_deli, "Ada")
+    
