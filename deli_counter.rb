418f7bf17c82ff360164ac2412ae6e32c9a588ab
Sarah Harrs <sarahharrs@gmail.com>
2019-06-28 23:23:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each.with_index(1) do |person|
-      current_line << " #{person}"
+    katz_deli.each.with_index(1) do |person,index|
+      current_line << " #{index}. #{person}"
     end
     puts current_line
   end
